06dd9f431ebcc554b25ba1d2271dcb5c3bec1c98
Alessandro Colace <ale.colace@gmail.com>
2025-08-06 16:08:44 +0200

    feat: improved schedule (13D/G) filing xml processor

--- app/scraper/xml_processor.py
+++ app/scraper/xml_processor.py
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
 def xml_to_dataframe_13f(xml_content):
     """
-    Parses the XML content and returns the data as a Pandas DataFrame.
+    Parses the XML content of a 13F filing and returns the data as a Pandas DataFrame.
     """
     soup_xml = BeautifulSoup(xml_content, "lxml")
 
@@ -60,7 +60,7 @@ def xml_to_dataframe_13f(xml_content):
 
 def xml_to_dataframe_schedule(xml_content):
     """
-    Parses the XML content and returns the data as a Pandas DataFrame.
+    Parses the XML content of a Schedule 13G/D filing and returns the data as a Pandas DataFrame.
     """
     soup_xml = BeautifulSoup(xml_content, "lxml")
 
@@ -73,27 +73,23 @@ def xml_to_dataframe_schedule(xml_content):
 
     data = []
 
-    for info_share in soup_xml.find_all(lambda tag: tag.name.endswith('formdata')):
-        company = info_share.find('issuername').text
-        cusip = info_share.find('issuercusip').text
+    form_data = soup_xml.find('formdata')
+    company = form_data.find(lambda tag: tag.name.endswith('issuername')).text
+    cusip = form_data.find(lambda tag: tag.name.endswith('issuercusip')).text
 
-        for reporting_person in soup_xml.find_all(lambda tag: tag.name.endswith('reportingpersoninfo')):
-            owner = reporting_person.find('reportingpersoncik').text
-            shares = reporting_person.find('sharedvotingpower').text
+    for reporting_person in soup_xml.find_all('coverpageheaderreportingpersondetails') or soup_xml.find_all('reportingpersoninfo'):
+        shares = reporting_person.find(lambda tag: tag.name.endswith('sharedvotingpower')).text.strip()
+        cik_tag = reporting_person.find(lambda tag: tag.name.endswith('reportingpersoncik'))
+        owner = cik_tag.text.strip() if cik_tag else reporting_person.find(lambda tag: tag.name.endswith('reportingpersonname')).text.strip()
 
-            data.append([
-                company,
-                cusip,
-                shares,
-                owner
-            ])
+        data.append([company, cusip, shares, owner])
 
     df = pd.DataFrame(data, columns=columns)
-
+    
     # Data cleaning
     df['CUSIP'] = df['CUSIP'].str.upper()
-    df['Company'] = df['Company'].str.strip().str.replace(r'\s+', ' ', regex=True)
-    df['Shares'] = pd.to_numeric(df['Shares'], errors='coerce')
+    df['Company'] = df['Company'].str.replace(r'\s+', ' ', regex=True)
+    df['Shares'] = pd.to_numeric(df['Shares'], errors='coerce').astype(int)
     df['Owner'] = df['Owner'].str.upper()
 
     return df
