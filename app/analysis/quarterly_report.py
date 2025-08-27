8e8e5fdada90e33d2ddc3e111f1f116c34f8bf8f
Alessandro Colace <ale.colace@gmail.com>
2025-08-27 17:35:19 +0200

    feat: add shares number in quarterly reports (this info is needed to compare 13F with 13D/G share updates between quarters)

--- app/analysis/quarterly_report.py
+++ app/analysis/quarterly_report.py
@@ -19,22 +19,22 @@ def generate_comparison(df_recent, df_previous):
         suffixes=('_recent', '_previous')
     )
 
-    df_comparison['Shares_recent'] = pd.to_numeric(df_comparison['Shares_recent'], errors='coerce').fillna(0).astype('int64')
+    df_comparison['Shares'] = pd.to_numeric(df_comparison['Shares_recent'], errors='coerce').fillna(0).astype('int64')
     df_comparison['Shares_previous'] = pd.to_numeric(df_comparison['Shares_previous'], errors='coerce').fillna(0).astype('int64')
     df_comparison['Value'] = pd.to_numeric(df_comparison['Value_recent'], errors='coerce').fillna(0).astype('int64')
     df_comparison['Value_previous'] = pd.to_numeric(df_comparison['Value_previous'], errors='coerce').fillna(0).astype('int64')
 
     df_comparison['Company'] = coalesce(df_comparison['Company_recent'], df_comparison['Company_previous'])
-    df_comparison['Price_per_Share'] = (coalesce(df_comparison['Value'] / df_comparison['Shares_recent'], df_comparison['Value_previous'] / df_comparison['Shares_previous']))
-    df_comparison['Delta_Shares'] = df_comparison['Shares_recent'] - df_comparison['Shares_previous']
+    df_comparison['Price_per_Share'] = (coalesce(df_comparison['Value'] / df_comparison['Shares'], df_comparison['Value_previous'] / df_comparison['Shares_previous']))
+    df_comparison['Delta_Shares'] = df_comparison['Shares'] - df_comparison['Shares_previous']
     df_comparison['Delta_Value'] = (df_comparison['Delta_Shares'] * df_comparison['Price_per_Share']).fillna(0).astype(int)
     df_comparison['Delta%'] = (df_comparison['Delta_Shares'] / df_comparison['Shares_previous']) * 100
 
     df_comparison['Delta'] = df_comparison.apply(
         lambda row: 
         'NEW' if row['Shares_previous'] == 0
-        else 'CLOSE' if row['Shares_recent'] == 0
-        else 'NO CHANGE' if row['Shares_recent'] == row['Shares_previous']
+        else 'CLOSE' if row['Shares'] == 0
+        else 'NO CHANGE' if row['Shares'] == row['Shares_previous']
         else format_percentage(row['Delta%'], True),
         axis=1
     )
@@ -56,17 +56,18 @@ def generate_comparison(df_recent, df_previous):
     df_comparison['Value'] = df_comparison['Value'].apply(format_value)
     df_comparison['Delta_Value'] = df_comparison['Delta_Value'].apply(format_value)
 
-    df_comparison = df_comparison[['CUSIP', 'Ticker', 'Company', 'Value', 'Portfolio%', 'Delta_Value', 'Delta']]
+    df_comparison = df_comparison[['CUSIP', 'Ticker', 'Company', 'Shares', 'Value', 'Delta_Value', 'Delta', 'Portfolio%']]
 
     # Final Total row
     total_row = pd.DataFrame([{
         'CUSIP': 'Total', 
         'Ticker': '', 
         'Company': '',
+        'Shares': '',
         'Value': format_value(total_portfolio_value),
-        'Portfolio%': format_percentage(100),
         'Delta_Value': format_value(total_delta_value),
-        'Delta': format_percentage(total_delta, True)
+        'Delta': format_percentage(total_delta, True),
+        'Portfolio%': format_percentage(100),
     }])
 
     return pd.concat([df_comparison, total_row], ignore_index=True)
