72795fb80dadd29ac6aaaf9ba03610109977387a
Alessandro Colace <ale.colace@gmail.com>
2025-08-30 11:29:20 +0200

    fix: daily fetcher

--- .github/scripts/fetcher.py
+++ .github/scripts/fetcher.py
@@ -1,9 +1,10 @@
-from app.main import run_all_funds_report, run_fetch_latest_schedules
-from app.utils.database import sort_stocks
 import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+from app.main import run_all_funds_report, run_fetch_latest_schedules
+from app.utils.database import sort_stocks
 
 
 if __name__ == "__main__":
