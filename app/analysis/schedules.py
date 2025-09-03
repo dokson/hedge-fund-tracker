e29bd86f30df54c71ba2a35ea085dbdbf075b187
Alessandro Colace <ale.colace@gmail.com>
2025-09-03 17:43:48 +0200

    feat: add Value to schedule filings using yfinance

--- app/analysis/schedules.py
+++ app/analysis/schedules.py
@@ -2,8 +2,11 @@ from app.scraper.xml_processor import xml_to_dataframe_schedule
 from app.tickers.resolver import resolve_ticker
 from app.utils.database import load_schedules_data
 from app.utils.pd import coalesce
+from app.utils.strings import format_value
+import datetime
 import numpy as np
 import pandas as pd
+import yfinance as yf
 
 
 def get_latest_schedule_filings_dataframe(schedule_filings, fund_denomination, cik):
@@ -32,7 +35,7 @@ def get_latest_schedule_filings_dataframe(schedule_filings, fund_denomination, c
             schedule_list.append(filtered_df)
         else:
             print(schedule_df)
-            print("⚠️ Hedge fund denomination or CIK not found inside filing.")
+            print("⚠️\u3000Hedge fund denomination or CIK not found inside filing.")
             return None
 
     schedule_filings_df = pd.concat(schedule_list, ignore_index=True)
@@ -40,7 +43,23 @@ def get_latest_schedule_filings_dataframe(schedule_filings, fund_denomination, c
     schedule_filings_df = schedule_filings_df.sort_values(by=['CUSIP', 'Filing_Date', 'Date'], ascending=False).drop_duplicates(subset='CUSIP', keep='first')
     schedule_filings_df = resolve_ticker(schedule_filings_df)
 
-    return schedule_filings_df[['CUSIP', 'Ticker', 'Company', 'Shares', 'Date']]
+    # Initialize 'Value' column before the loop to prevent KeyError
+    schedule_filings_df['Value'] = pd.NA
+    for index, row in schedule_filings_df.iterrows():
+        ticker = row['Ticker']
+        date = row['Date'].date()
+        # yfinance 'end' parameter is exclusive. To get a single day, we need the next day as the end.
+        price_data = yf.download(tickers=ticker, start=date, end=date+datetime.timedelta(days=1), auto_adjust=False, progress=False)
+        if not price_data.empty:
+            # Considering daily price as the average of daily high and daily low
+            price_per_share = (price_data['High'].iloc[0].item() + price_data['Low'].iloc[0].item()) / 2
+            schedule_filings_df.at[index, 'Value'] = price_per_share * row['Shares']
+        else:
+            print(f"⚠️\u3000Could not find price for {ticker} on {date}.")
+
+    schedule_filings_df['Value'] = format_value(schedule_filings_df['Value'])
+
+    return schedule_filings_df[['CUSIP', 'Ticker', 'Company', 'Shares', 'Value', 'Date']]
 
 
 def update_last_quarter_with_schedules(last_quarter_df):
